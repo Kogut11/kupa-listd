95a0fd2265013b6ccf011f0e8f7e46e097ddcba6
symmetrikas <zagraianmanu@gmail.com>
2026-04-09 16:18:09 +0300

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 2;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 125) {
         return 0;
     }
-    if (rank > 100 && percent < 100) {
+    if (rank > 125 && percent < 100) {
         return 0;
     }
 
