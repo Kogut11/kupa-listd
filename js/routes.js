c56423187ebfc1ee83fbcb36a74f4284f6d9b151
symmetrikas <zagraianmanu@gmail.com>
2026-08-06 16:18:42 +0300

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,19 +1,11 @@
 import List from './pages/List.js';
-import Legacy from './pages/Legacy.js';
-
 import Leaderboard from './pages/Leaderboard.js';
-import LegacyLeaderboard from './pages/LegacyLeaderboard.js';
-
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js';
 
 export default [
     { path: '/', component: List },
-    { path: '/legacy', component: Legacy },
-
     { path: '/leaderboard', component: Leaderboard },
-    { path: '/legacyleaderboard', component: LegacyLeaderboard },
-
     { path: '/roulette', component: Roulette },
     { path: '/listpacks', component: ListPacks },
 ];
