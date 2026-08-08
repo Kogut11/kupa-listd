c6ddea2ced82621c76a55244ac242c33d941225f
symmetrikas <zagraianmanu@gmail.com>
2026-08-08 18:11:29 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -89,12 +89,21 @@ export async function fetchEditors() {
         }
     }
 
-
-export async function fetchLeaderboard() {
-    const list = await fetchList();
+export async function fetchLeaderboard(legacy = false) {
+    const list = legacy
+        ? await fetchLegacy()
+        : await fetchList();
 
     const scoreMap = {};
     const errs = [];
+
+    if (!list) {
+        return [
+            [],
+            [`Failed to load ${legacy ? "legacy" : "main"} list.`]
+        ];
+    }
+
     list.forEach(([level, err], rank) => {
         if (err) {
             errs.push(err);
@@ -103,39 +112,59 @@ export async function fetchLeaderboard() {
 
         // Verification
         const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+            (u) =>
+                u.toLowerCase() ===
+                level.verifier.toLowerCase()
         ) || level.verifier;
+
         scoreMap[verifier] ??= {
             verified: [],
             completed: [],
             progressed: [],
         };
+
         const { verified } = scoreMap[verifier];
+
         verified.push({
             rank: rank + 1,
             level: level.name,
-            score: score(rank + 1, 100, level.percentToQualify),
+            score: score(
+                rank + 1,
+                100,
+                level.percentToQualify
+            ),
             link: level.verification,
         });
 
         // Records
         level.records.forEach((record) => {
             const user = Object.keys(scoreMap).find(
-                (u) => u.toLowerCase() === record.user.toLowerCase(),
+                (u) =>
+                    u.toLowerCase() ===
+                    record.user.toLowerCase()
             ) || record.user;
+
             scoreMap[user] ??= {
                 verified: [],
                 completed: [],
                 progressed: [],
             };
-            const { completed, progressed } = scoreMap[user];
+
+            const { completed, progressed } =
+                scoreMap[user];
+
             if (record.percent === 100) {
                 completed.push({
                     rank: rank + 1,
                     level: level.name,
-                    score: score(rank + 1, 100, level.percentToQualify),
+                    score: score(
+                        rank + 1,
+                        100,
+                        level.percentToQualify
+                    ),
                     link: record.link,
                 });
+
                 return;
             }
 
@@ -143,11 +172,50 @@ export async function fetchLeaderboard() {
                 rank: rank + 1,
                 level: level.name,
                 percent: record.percent,
-                score: score(rank + 1, record.percent, level.percentToQualify),
+                score: score(
+                    rank + 1,
+                    record.percent,
+                    level.percentToQualify
+                ),
                 link: record.link,
             });
         });
     });
+
+    const leaderboard = Object.entries(scoreMap).map(
+        ([user, scores]) => {
+            const {
+                verified,
+                completed,
+                progressed
+            } = scores;
+
+            const total = [
+                verified,
+                completed,
+                progressed
+            ]
+                .flat()
+                .reduce(
+                    (prev, cur) => prev + cur.score,
+                    0
+                );
+
+            return {
+                user,
+                total: round(total),
+                ...scores,
+            };
+        }
+    );
+
+    return [
+        leaderboard.sort(
+            (a, b) => b.total - a.total
+        ),
+        errs
+    ];
+}
 
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
