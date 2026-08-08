2085543d08ac47e87d1c3d03500f57390c6c304a
symmetrikas <zagraianmanu@gmail.com>
2026-08-08 18:04:20 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -103,15 +103,6 @@ computed: {
 
         this.loading = false;
     },
-
-    getOriginalRank(level) {
-        let index = this.list.findIndex(
-            (item) => item[0] && item[0].id === level.id
-        );
-
-        return index >= 0 ? index + 1 : this.selected + 1;
-    },
-},
     getOriginalRank(level) {
       let index = this.list.findIndex(
         (item) => item[0] && item[0].id === level.id
